6cb310b9cfbeadaa55165c9b242c2fe661f23599
Leo <kychancv@connect.ust.hk>
2023-04-11 19:18:14 +0800

    Basic styling

--- start.js
+++ start.js
@@ -1,10 +1,13 @@
+ace.require("ace/ext/language_tools");
 let editor = ace.edit("editor");
-editor.setTheme("ace/theme/monokai");
+editor.setTheme("ace/theme/cobalt");
 editor.session.setMode("ace/mode/javascript");
 editor.setOptions({
-    enableBasicAutocompletion: true
+    enableBasicAutocompletion: true,
+    enableLiveAutocompletion: true
 });
 
+/*
 let defaultCode = 
 `function foo(n) {
     let ans = 0;
@@ -16,13 +19,35 @@ let defaultCode =
 
 foo(15);
 `
+*/
 
-editor.setValue(defaultCode);
+let defaultCode = 
+`// You are given the following function:
+// hit() -- Hit the door once
 
-let button = document.getElementById("submit");
+for () {
+
+}
+`
+
+/*
+let answer = 
+`// You are given the following function:
+// hit() -- Hit the door once
+
+for (let i = 0; i < 100; i++) {
+    hit();
+}
+`
+*/
+
+editor.session.setValue(defaultCode);
+
+let submitButton = document.getElementById("submit");
+let resetButton = document.getElementById("reset")
 let outputBox = document.getElementById("output");
 
-button.addEventListener('click', function() {
+submitButton.addEventListener('click', function() {
     let code = editor.getValue();
     try {
         let result = eval(code);
@@ -30,4 +55,10 @@ button.addEventListener('click', function() {
     } catch (error) {
         outputBox.innerText = "There is something wrong in your code.";
     }
+}, false);
+
+resetButton.addEventListener('click', function() {
+    try {
+        editor.session.setValue(defaultCode);
+    } catch (error) {}
 }, false);
